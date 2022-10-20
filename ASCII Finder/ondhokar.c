da67f8cb671cbaca4771b1ac12e6fa659689254c
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-10-21 00:24:04 +0600

    initial commit

---
+++ ASCII Finder/ondhokar.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    // %d displays the integer value of a character
+    // %c displays the actual character
+
+    printf("ASCII value is %d",c);
+
+    return 0;
+}
---
+++ Percentage Shower/lol.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char a;
+    int b;
+    printf("Enter an number with percentage : ");
+    scanf("%d%c",&b,&a);
+    printf("number is %d and percentage is %c",b,a);
+return 0;
+}
